2357898d296d5814bf8e2a7c06fb89de325ffa8b
Low <khcy4lwi@nottingham.edu.my>
2016-03-01 17:11:35 +0800

    rating_bar

--- Desktop/grp10/app/src/main/java/my/edu/khcy4jwnnottingham/crisisalertwithvisualandlocationsupport/Camera.java
+++ Desktop/grp10/app/src/main/java/my/edu/khcy4jwnnottingham/crisisalertwithvisualandlocationsupport/Camera.java
@@ -4,22 +4,26 @@ import android.content.Intent;
 import android.graphics.drawable.Drawable;
 import android.net.Uri;
 import android.os.Bundle;
+import android.os.Environment;
 import android.provider.MediaStore;
 import android.support.design.widget.FloatingActionButton;
 import android.support.design.widget.Snackbar;
 import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.Toolbar;
+import android.util.Log;
 import android.view.View;
 import android.widget.ImageView;
-
+import android.widget.Toast;
 import com.google.android.gms.appindexing.Action;
 import com.google.android.gms.appindexing.AppIndex;
 import com.google.android.gms.common.api.GoogleApiClient;
 
 import java.io.File;
+import java.io.IOException;
+import java.text.SimpleDateFormat;
+import java.util.Date;
 
 public class Camera extends AppCompatActivity {
-
     /**
      * ATTENTION: This was auto-generated to implement the App Indexing API.
      * See https://g.co/AppIndexing/AndroidStudio for more information.
@@ -27,25 +31,24 @@ public class Camera extends AppCompatActivity {
     private GoogleApiClient client;
     static final int CAM_REQUEST = 1;
     ImageView imageView;
+    private static final int CAPTURE_IMAGE_ACTIVITY_REQUEST_CODE = 100;
+    private Uri fileUri;
+    String mCurrentPhotoPath;
 
     @Override
-    protected void onCreate(Bundle savedInstanceState) {
+    public void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_camera);
-        Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
-        setSupportActionBar(toolbar);
 
-    FloatingActionButton fab = (FloatingActionButton) findViewById(R.id.fab);
-    fab.setOnClickListener(new View.OnClickListener()
-                           {
-                               @Override
-                               public void onClick(View view) {
-                                   Snackbar.make(view, "Replace with your own action", Snackbar.LENGTH_LONG)
-                                           .setAction("Action", null).show();
-                               }
-                           }
-    );
-}
+        FloatingActionButton fab = (FloatingActionButton) findViewById(R.id.fab);
+        fab.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Snackbar.make(view, "Replace with your own action", Snackbar.LENGTH_LONG)
+                        .setAction("Action", null).show();
+            }
+        });
+    }
     public void cam(View v) {
         Intent camera_intent = new Intent(MediaStore.ACTION_IMAGE_CAPTURE);
         File file = getFile();
@@ -53,12 +56,10 @@ public class Camera extends AppCompatActivity {
         startActivityForResult(camera_intent, CAM_REQUEST);
     }
 
-    private File getFile()
-    {
+    private File getFile() {
         File folder = new File("sdcard/camera_app");
 
-        if(!folder.exists())
-        {
+        if (!folder.exists()) {
             folder.mkdir();
         }
 
@@ -66,9 +67,36 @@ public class Camera extends AppCompatActivity {
         return image_file;
 
     }
-
-    /*protected void onActivityResult(int requestCode, int resultCode, Intent data) {
-        String path =  "sdcard/camera_app/cam_image.jpg";
+    protected void onActivityResult(int requestCode, int resultCode, Intent data) {
+        String path = "sdcard/camera_app/cam_image.jpg";
         imageView.setImageDrawable(Drawable.createFromPath(path));
-    }*/
+    }
 }
+
+
+
+/*class one {
+
+    public void cam(View v) {
+        Intent camera_intent = new Intent(MediaStore.ACTION_IMAGE_CAPTURE);
+        File file = getFile();
+        camera_intent.putExtra(MediaStore.EXTRA_OUTPUT, Uri.fromFile(file));
+        startActivityForResult(camera_intent, CAM_REQUEST);
+    }
+
+    private File getFile() {
+        File folder = new File("sdcard/camera_app");
+
+        if (!folder.exists()) {
+            folder.mkdir();
+        }
+
+        File image_file = new File(folder, "cam_image.jpg");
+        return image_file;
+
+    }
+    protected void onActivityResult(int requestCode, int resultCode, Intent data) {
+        String path = "sdcard/camera_app/cam_image.jpg";
+        imageView.setImageDrawable(Drawable.createFromPath(path));
+    }
+}*/
